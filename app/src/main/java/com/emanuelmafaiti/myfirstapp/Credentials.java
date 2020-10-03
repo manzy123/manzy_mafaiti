7fe257f710cacca78337f809edcd3a0194c1d70a
manzy123 <emmanuelmafaiti@gmail.com>
2020-10-03 23:21:49 +1200

    this my first app in Git Hub

---
+++ app/src/main/java/com/emanuelmafaiti/myfirstapp/Credentials.java
@@ -0,0 +1,14 @@
+package com.emanuelmafaiti.myfirstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Credentials extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_credentials);
+    }
+}
